849d6b962378787fbf093eab676af1aef2e45823
MOHIT SRIWASTAVA <mohit67621@gmail.com>
2026-07-02 12:26:20 +0530

    Create alias.ts

---
+++ alias.ts
@@ -0,0 +1,13 @@
+type Details = {name: string, marks: number, address? : string};
+// Details is kind of like an alias or a nickname to {name: string, marks: number, address? : string}
+
+const result1 : Details = {
+    name: "mohit",
+    marks: 98
+}
+
+
+const result2 : Details = {
+    name: "rohit",
+    marks: 95
+}
